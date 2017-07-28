d0be68dc7c782192fcf8568345f4d0d74231a5ab
Danny <sdanielabelvedere@gmail.com>
2017-07-28 14:11:50 -0300

    few details

--- public/js/busqueda.js
+++ public/js/busqueda.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
  $(".input-search").attr({
   value: QueryParameters.getUrlParameter("search"),
 	});
+
   $.get("/api/items?q=" + QueryParameters.getUrlParameter("search"), function (res) {
     $("#template")
       .tmpl(res.items)
